02caf86925b22980f4216ee97e03658c3f524e4f
Vinicius Linck <vinnylinck@gmail.com>
2016-02-23 21:59:34 -0300

    React Flux: Remove Duplicate Code with Mixins

--- flux-archie-es5/lesson07/src/js/components/cart/app-cart.js
+++ flux-archie-es5/lesson07/src/js/components/cart/app-cart.js
@@ -15,6 +15,9 @@ var Cart = React.createClass({
     componentWillMount:function(){
         AppStore.addChangeListener(this._onChange)
     },
+    componentWillUnmount:function(){
+        AppStore.removeChangeListener(this._onChange)
+    },
     _onChange: function(){
         this.setState(cartItems())
     },
