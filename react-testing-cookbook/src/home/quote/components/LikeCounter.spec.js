340157593d400f4478e97e91d06e7da969fe9fe4
Vinicius Linck <vinnylinck@gmail.com>
2016-04-04 20:40:07 -0300

    React Testing: Reusing test boilerplate

--- react-testing-cookbook/src/home/quote/components/LikeCounter.spec.js
+++ react-testing-cookbook/src/home/quote/components/LikeCounter.spec.js
@@ -14,4 +14,22 @@ describe('LikeCounter', () => {
     const expected = 'a';
     expect(actual).toEqual(expected);
   });
+
+
+  describe('isActive', () => {
+
+    function renderLikeCounter(isActive) {
+      const renderer = TestUtils.createRenderer();
+      renderer.render(<LikeCounter count={5} isActive={isActive} />);
+      return renderer.getRenderOutput().props.className.includes('LikeCounter--active');
+    }
+
+    it('should show the like count as active', () => {
+      expect(renderLikeCounter(true)).toEqual(true);
+    });
+
+    it('should show the like count as inactive', () => {
+      expect(renderLikeCounter(false)).toEqual(false);
+    });
+  });
 });
