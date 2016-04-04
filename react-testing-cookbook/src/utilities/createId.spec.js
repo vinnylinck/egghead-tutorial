21c9a153e9dd0b0ff8e7e225a3830d63a71951fc
Vinicius Linck <vinnylinck@gmail.com>
2016-04-04 20:17:39 -0300

    React Testing: Utility modules

---
+++ react-testing-cookbook/src/utilities/createId.spec.js
@@ -0,0 +1,10 @@
+import expect from 'expect';
+import createId from './createId';
+
+describe('createId', () => {
+    it('should convert a description to a unique id', () => {
+        const actual = createId(123, 'Cool example description');
+        const expected = '123-cool-example';
+        expect(actual).toEqual(expected);
+    });
+});
